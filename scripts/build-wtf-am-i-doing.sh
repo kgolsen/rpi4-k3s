03e6826ef79b144d086643eed674d23b79642794
kgolsen <dxo50DW7>
2020-05-26 10:09:48 -0700

    Fleshing out build script some more

--- scripts/build-wtf-am-i-doing.sh
+++ scripts/build-wtf-am-i-doing.sh
@@ -71,9 +71,63 @@ mount -v -o offset="$(echo "${parts[1]}"|awk '{print $1}')",sizelimit="$(echo "$
 git clone --depth 1 git://github.com/raspberrypi/firmware.git /tmp/firmware
 mv /tmp/firmware/boot/* "${BOOT}"
 touch "${BOOT}/ssh"
+# Add container capabilities to the kernel boot command
+CMDLINE=$(cat "${BOOT}/cmdline.txt")
+echo "${CMDLINE} cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory" | tee "${BOOT}/cmdline.txt"
+# Reduce GPU memory to the minimum allowed, as we are running headless
+echo "gpu_mem=16" | cat - "${BOOT}/config.txt" | tee "${BOOT}/config.txt"
+# Don't load snd_bcm2835, as we are running headless
+sed -i -e 's/audio=on/audio=off/' "${BOOT}/config.txt"
 
 # Bootstrap the Raspbian root
 debootstrap --no-check-gpg --foreign --arch=armhf buster "${ROOT}" http://archive.raspbian.org/raspbian
 cp /usr/bin/qemu-arm-static "${ROOT}/usr/bin/"
 mount -o remount -t proc /proc "${ROOT}/proc/"
 chroot "${ROOT}" /debootstrap/debootstrap --second-stage
+
+# chroot to Raspbian and setup SSH
+cat << EOF | chroot "${ROOT}"
+echo "k3s-base" > /etc/hostname
+sed -i -e 's/raspberrypi/k3s-base/' /etc/hosts
+mkdir -p /home/pi/.ssh
+cd /home/pi/.ssh
+ssh-keygen -f k3s-masterkey -t rsa -b 4096 -N '' -m PEM -q
+mv k3s-masterkey k3s-masterkey.pem
+cat k3s-masterkey.pub > authorized_keys
+chmod 644 authorized_keys
+chmod 400 k3s-masterkey.pem
+chmod 700 /home/pi/.ssh
+chown -R pi:pi /home/pi/.ssh
+EOF
+cp "${ROOT}/home/pi/.ssh/k3s-masterkey*" /var/local/
+
+# chroot to Raspbian, setup rc.local to install k3sup
+cat << EOF | chroot "${ROOT}"
+cat << EORC | tee /etc/rc.local &> /dev/null
+#!/bin/bash
+# update and upgrade everything
+apt-get update
+apt-get upgrade -y
+apt-get full-upgrade -y
+
+# pull k3sup installer and run
+wget -q --compression=auto https://get.k3sup.dev -O /usr/local/bin/install-k3sup.sh
+chmod +x /usr/local/bin/*.sh
+/usr/local/bin/install-k3sup.sh && rm /usr/local/bin/install-k3sup.sh
+
+# disable password auth for SSH
+sed -i -e 's/#PasswordAuthentication yes/PasswordAuthentication no/' /etc/ssh/sshd_config
+
+# disable swapfile
+systemctl disable dphys-swapfile.service
+systemctl stop dphys-swapfile.service
+chmod -x /etc/rc.local
+EORC
+chmod +x /etc/rc.local
+exit
+EOF
+
+# Unmount and copy image to volume
+umount "${BOOT}"
+umount "${ROOT}"
+cp "${IMG_FILE}" /var/local/
